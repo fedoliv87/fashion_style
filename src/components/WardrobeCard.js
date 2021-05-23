fbe025cfae7ae375361b54b1a879f7c5486d9b4a
Federico Liviabella <federico.liviabella@gmail>
2021-05-23 16:42:35 +0200

    Delete action for wardrobeCard

--- src/components/WardrobeCard.js
+++ src/components/WardrobeCard.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import {Button, Card} from "react-bootstrap";
 import Axios from 'axios'
+import config from "../config.json";
+import {Auth} from "aws-amplify";
 
 function WardrobeCard(props){
 
     const handleClickDelete = event => {
         console.log(event)
         console.log(props.url)
+        Auth.currentSession()
+            .then( res => {
+                //console.log(`myAccessToken: ${JSON.stringify(res.getIdToken())}`)
+                let jwt = res.getIdToken().getJwtToken()
+                console.log('myJwt: ' + jwt)
+                console.log(config.apiBaseUrl+config.wardrobe.upload)
+
+                //UPLAOD
+                Axios.delete(
+                    config.apiBaseUrl+config.wardrobe.update,
+                    { headers: {Authorization: jwt}}
+                )
+                .then( (res) => {
+                        console.log(res)
+                        console.log(res.data)
+                    }
+                )
+                .catch( (err) => {
+                        console.log(err)
+                    }
+                )
+
+            })
+            .catch( err => {
+                console.log(err)
+            })
     }
 
     return(
