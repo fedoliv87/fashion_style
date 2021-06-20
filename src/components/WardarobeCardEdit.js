a0ef7c978f7cc037b4845331240d6c834921a643
Federico Liviabella <federico.liviabella@gmail>
2021-06-20 15:50:19 +0200

    wardrobe card edit

---
+++ src/components/WardarobeCardEdit.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {Modal} from "react-bootstrap";
+import {Auth} from "aws-amplify";
+import config from "../config.json";
+import Axios from "axios";
+
+class WardarobeCardEdit extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            info : null
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.props.name === null){
+            return
+        }
+
+        Auth.currentSession()
+            .then( res => {
+                let jwt = res.getIdToken().getJwtToken()
+                console.log(config.apiBaseUrl+config.wardrobe.info)
+
+                //INFO
+                Axios.get(
+                    config.apiBaseUrl+config.wardrobe.info,
+                    {
+                        headers: {Authorization: jwt},
+                        params: {imgId: this.props.name}
+                    }
+                )
+                    .then( (res) => {
+                            console.log(res)
+                            this.setState({info: res.data})
+                        }
+                    )
+                    .catch( (err) => {
+                            console.log(err)
+                        }
+                    )
+            })
+            .catch( err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+        return (<div>
+            <Modal
+                {...this.props}
+                size='lg'
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter"> INFORMATION </Modal.Title>
+                </Modal.Header>
+
+            </Modal>
+        </div>)
+    }
+
+}
+export default WardarobeCardEdit
